3f43a619c8ed0e32a104ac472328b8404e41b4d6
dlaurencio <106186545+dlaurencio@users.noreply.github.com>
2025-08-14 12:51:21 -0400

    Added char counter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import get_num_words
+from stats import get_char_count
 
 def main():
     get_num_words()
+    get_char_count()
 
 main()
--- stats.py
+++ stats.py
@@ -8,4 +8,14 @@ def get_num_words():
     book_list = book_text.split()
     word_count = len(book_list)
     print(f"{word_count} words found in the document")
-    
+
+def get_char_count():
+    book_text = get_book_text("books/frankenstein.txt")
+    char_dict = {}
+    for char in book_text:
+        charl = char.lower()
+        if charl not in char_dict:
+            char_dict[charl] = 1
+        else:
+            char_dict[charl] += 1
+    print(char_dict)
